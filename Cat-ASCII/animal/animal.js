c9db66374b4c1cea8c4c4e254680180794206de1
Ahsan Ibadur <116834969+Dfuums@users.noreply.github.com>
2024-04-04 21:08:52 +0000

    Your commit message here

---
+++ Cat-ASCII/animal/animal.js
@@ -0,0 +1,104 @@
+const cowsay = require('cowsay');
+
+function catSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a cat!", f: "cat" }));
+}
+
+function dogSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a dog!", f: "dog" }));
+}
+
+function bunnySaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a bunny!", f: "bunny" }));
+}
+
+function elephantSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm an elephant!", f: "elephant" }));
+}
+
+function hippoSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a hippo!", f: "hippo" }));
+}
+
+function pigSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a pig!", f: "pig" }));
+}
+
+function koalaSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a koala!", f: "koala" }));
+}
+
+function tigerSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a tiger!", f: "tiger" }));
+}
+
+function mouseSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a mouse!", f: "mouse" }));
+}
+
+function bearSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a bear!", f: "bear" }));
+}
+
+function owlSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm an owl!", f: "owl" }));
+}
+
+function snakeSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a snake!", f: "snake" }));
+}
+
+function penguinSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a penguin!", f: "penguin" }));
+}
+
+function frogSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a frog!", f: "frog" }));
+}
+
+function giraffeSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a giraffe!", f: "giraffe" }));
+}
+
+function monkeySaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a monkey!", f: "monkey" }));
+}
+
+function horseSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a horse!", f: "stegosaurus" }));
+}
+
+function stegosaurusSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a stegosaurus!", f: "stegosaurus" }));
+}
+
+function dragonSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a dragon!", f: "dragon" }));
+}
+
+function unicornSaysHello() {
+  console.log(cowsay.say({ text: "Hello, I'm a unicorn!", f: "unicorn" }));
+}
+
+module.exports = {
+  catSaysHello,
+  dogSaysHello,
+  bunnySaysHello,
+  elephantSaysHello,
+  hippoSaysHello,
+  pigSaysHello,
+  koalaSaysHello,
+  tigerSaysHello,
+  mouseSaysHello,
+  bearSaysHello,
+  owlSaysHello,
+  snakeSaysHello,
+  penguinSaysHello,
+  frogSaysHello,
+  giraffeSaysHello,
+  monkeySaysHello,
+  horseSaysHello,
+  stegosaurusSaysHello,
+  dragonSaysHello,
+  unicornSaysHello,
+};
---
+++ Cat-ASCII/index.js
@@ -0,0 +1,7 @@
+const cowsay = require('cowsay');
+
+function helloWorldCat() {
+  console.log(cowsay.say({ text: "hello world", f: "cat" }));
+}
+
+module.exports = helloWorldCat;
